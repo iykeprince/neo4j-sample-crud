c83fd2546d15e69e278a634821b7c6866253fc59
iykeprince <iykeprince48@gmail.com>
2021-10-05 23:58:48 +0100

    Initialize commmit. Added Neo4j

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var path = require('path')
+var logger = require('morgan')
+var bodyParser = require('body-parser')
+var neo4j = require('neo4j-driver')
+
+var app = express()
+
+//view engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+var driver = neo4j.driver('neo4j://localhost', neo4j.auth.basic('neo4j', 'neo4j'))
+var session = driver.session();
+
+app.get('/', function(req, res){
+    res.send('it works')
+})
+
+app.listen(3000)
+console.log('server started at 3000')
+
+module.exports = app
